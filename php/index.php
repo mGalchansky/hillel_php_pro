aeb72d8b68bc6a9984f49dfaaf06ed92515e09e2
Mikhailo <mgalchansky@gmail.com>
2024-11-04 22:24:20 +0200

    add ValueObject class

--- php/index.php
+++ php/index.php
@@ -1,11 +1,53 @@
-<html>
-    <head>
-        <title>Hello World</title>
-    </head>
+<?php
 
-    <body>
-       <h1> <?php
-           echo "Hello, World!";
-           ?></h1>
-    </body>
-</html>
+class ValueObject
+    {
+    private int $red;
+    private int $green;
+    private int $blue;
+
+    public function __construct(int $red, int $green, int $blue)
+    {
+        $this->setRed($red);
+        $this->setGreen($green);
+        $this->setBlue($blue);
+
+    }
+
+    public function setRed(int $red): void
+    {
+        if ($red >= 0 and $red <= 255)
+        $this->red = $red;
+        else throw new Exception('value must be between 0 and 255');
+    }
+    public function setGreen(int $green): void
+    {
+        if ($green >= 0 and $green <= 255)
+            $this->green = $green;
+        else throw new Exception('value must be between 0 and 255');
+    }
+    public function setBlue(int $blue): void
+    {
+        if ($blue >= 0 and $blue <= 255)
+            $this->blue = $blue;
+        else throw new Exception('value must be between 0 and 255');
+    }
+    public function getRed(): int
+    {
+        return $this->red;
+    }
+    public function getGreen(): int
+    {
+        return $this->green;
+    }
+    public function getBlue(): int
+    {
+        return $this->blue;
+    }
+    }
+
+
+    $colors = new ValueObject(255, 0,10);
+echo $colors->getRed();
+echo $colors->getGreen();
+echo $colors->getBlue();
