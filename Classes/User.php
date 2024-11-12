b618b40244d93236838fc24baf2c0e15bf458565
Mikhailo <mgalchansky@gmail.com>
2024-11-13 00:30:24 +0200

    Error class eception

--- Classes/User.php
+++ Classes/User.php
@@ -1,15 +1,35 @@
 <?php
-
 namespace Classes;
-
 class User
 {
-    private string $name = 'Gena';
+    private $name;
     private $email;
     private $age;
 
-    public function test()
+    public function __call($name, $arguments)
     {
-        return $this->name;
+        if (!method_exists($this, $name)) {
+            throw new MyExeption('Method ' . $name . ' does not exist');
+        }
+        return call_user_func_array([$this, $name], $arguments);
+
     }
+
+    private function setName($name)
+    {
+        $this->name = $name;
+        echo 'Name: ' . $name;
+    }
+
+    private function setAge($age)
+    {
+        $this->age = $age;
+        echo 'Age: ' . $age;
+    }
+
+    public function getAll(): array
+    {
+        return [0 => $this->name, 1 => $this->age, 2 => $this->email];
+    }
+
 }
--- index.php
+++ index.php
@@ -3,6 +3,14 @@ require_once __DIR__ . '/Classes/User.php';
 
 use Classes\User;
 
-$test = new User();
+try {
+    $test = new User();
 
-var_dump($test);
+    $test->setName('Anton');
+    $test->setAge(22);
+    $test->setEmail('anton@gmail.com');
+
+    var_dump($test->getAll());
+} catch (MyException $e) {
+    echo $e->getMessage();
+}
