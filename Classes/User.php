1061a81e0b22e68457b5675d0535a55fe5456e05
Mikhailo <mgalchansky@gmail.com>
2024-11-12 18:46:38 +0200

    vse ponovoi

---
+++ Classes/User.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Classes;
+
+class User
+{
+private string $name = 'Gena';
+private $email;
+private $age;
+
+public function test(){
+    return $this->name;
+}
+}
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+#require_once 'Classes\User.php';
+
+use Classes\User;
+
+$test = new User();
+
+var_dump($test);
